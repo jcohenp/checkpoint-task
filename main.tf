19eaacb677ddb4704de4ef318da46e92db42249d
julien cohen <julien.cohn1993@gmail.com>
2024-01-26 02:53:00 +0200

    add jenkins image

--- main.tf
+++ main.tf
@@ -90,3 +90,154 @@ resource "kubernetes_deployment" "messages_broker" {
     }
   }
 }
+
+resource "kubernetes_deployment" "jenkins_master" {
+  metadata {
+    name      = "jenkins-master"
+
+    labels = {
+      app = "jenkins-master"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "jenkins-master"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "jenkins-master"
+        }
+      }
+
+      spec {
+        volume {
+          name = "jenkins-local-home"
+
+          host_path {
+            path = "/tmp"
+          }
+        }
+
+        container {
+          name  = "jenkins"
+          image = "jcohenp/jenkins"
+
+          port {
+            container_port = 8080
+          }
+
+          port {
+            container_port = 50000
+          }
+
+          volume_mount {
+            mount_path = "/var/jenkins_home"
+            name       = "jenkins-local-home"
+          }
+          security_context {
+            run_as_user = 0  # 0 represents the root user
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "jenkins_service" {
+  metadata {
+    name      = "jenkins-service"
+  }
+
+  spec {
+    selector = {
+      app = "jenkins-master"
+    }
+
+    type = "LoadBalancer"
+
+    port {
+      name        = "http"
+      port        = 8080
+      target_port = 8080
+    }
+
+    port {
+      name        = "jcli"
+      port        = 50000
+      target_port = 50000
+    }
+  }
+}
+
+
+# resource "kubernetes_deployment" "jenkins" {
+#   metadata {
+#     name = "jenkins"
+#   }
+
+#   spec {
+#     replicas = 1
+
+#     selector {
+#       match_labels = {
+#         app = "jenkins"
+#       }
+#     }
+
+#     template {
+#       metadata {
+#         labels = {
+#           app = "jenkins"
+#         }
+#       }
+
+#       spec {
+#         container {
+#           name  = "jenkins"
+#           image = "jenkins/jenkins:lts"
+
+#           port {
+#             container_port = 8080
+#           }
+
+#           port {
+#             container_port = 50000
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
+
+# resource "kubernetes_service" "jenkins" {
+#   metadata {
+#     name = "jenkins"
+#   }
+
+#   spec {
+#     selector = {
+#       app = kubernetes_deployment.jenkins.spec[0].template[0].metadata[0].labels.app
+#     }
+
+#     port {
+#       name        = "http"
+#       port        = 8080
+#       target_port = 8080
+#     }
+
+#     port {
+#       name        = "jcli"
+#       port        = 50000
+#       target_port = 50000
+#     }
+
+#     type = "LoadBalancer"
+#   }
+# }
